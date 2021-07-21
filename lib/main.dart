7df228da93422c8dc781c3e6e995093a18cb0ddd
utrodus <come.programmer@gmail.com>
2021-07-21 14:47:20 +0700

    Progress Convert Home Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:doctor_appointments/views/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,57 +11,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Doctor Appointments',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+      theme:
+          ThemeData(primarySwatch: Colors.blue, fontFamily: "Poppins-Regular"),
+      home: HomeScreen(),
     );
   }
 }
---
+++ lib/views/home_screen.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.fromLTRB(27, 30, 27, 35),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Text(
+                "Welcome Utrodus,",
+                style: TextStyle(fontSize: 15, color: Colors.black),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 7, 0, 0),
+                child: Text(
+                  "Find your doctor",
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontFamily: "Poppins-SemiBold",
+                      color: Colors.black),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 16, 0, 0),
+                child: TextField(
+                  enabled: false,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: BorderSide.none),
+                      filled: true,
+                      hintStyle:
+                          TextStyle(color: Color(0xff6B779A), fontSize: 14),
+                      suffixIcon: Icon(Icons.search),
+                      contentPadding: EdgeInsets.only(
+                          left: 15, bottom: 11, top: 11, right: 15),
+                      hintText: "Search For Doctor"),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(0, 25, 0, 0),
+                padding: EdgeInsets.fromLTRB(16, 20, 16, 0),
+                width: double.infinity,
+                height: 340,
+                decoration: BoxDecoration(
+                    color: Color(0xFF3E64FF),
+                    borderRadius: BorderRadius.all(Radius.circular(12)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color(0xFF3E64FF).withOpacity(0.3),
+                        offset: const Offset(
+                          0.0,
+                          15.0,
+                        ),
+                        blurRadius: 25.0,
+                        spreadRadius: 0.5,
+                      ),
+                    ]),
+                child: Stack(
+                  children: [
+                    Text(
+                      "Covid-19 Healthcare",
+                      softWrap: true,
+                      style: TextStyle(
+                          fontSize: 25,
+                          fontFamily: "Poppins-SemiBold",
+                          color: Colors.white),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 45.0),
+                      child: Text(
+                        "Book your next online appointments",
+                        softWrap: true,
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontFamily: "Poppins-SemiBold",
+                            color: Colors.white),
+                      ),
+                    ),
+                    Align(
+                        alignment: Alignment.bottomCenter,
+                        child: SvgPicture.asset("assets/svg/doctors.svg"))
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 32, 0, 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Popular Doctor",
+                      style: TextStyle(
+                          fontSize: 17,
+                          fontFamily: "Poppins-Bold",
+                          color: Colors.black),
+                    ),
+                    TextButton(
+                      onPressed: () => {},
+                      child: Text(
+                        "View All",
+                        style: TextStyle(
+                            fontSize: 14,
+                            fontFamily: "Poppins-Medium",
+                            color: Colors.black),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                width: 180,
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(20, 15, 20, 20),
+                  child: Column(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.all(20),
+                        width: 72,
+                        height: 72,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                              image: NetworkImage(
+                                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnLnP_dX6SiNEaqhA_MiXBlUAZSWEXRvds-A&usqp=CAU'),
+                              fit: BoxFit.cover),
+                        ),
+                      ),
+                      Text("Dr. Bellamy N"),
+                      Text("Viralogist"),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.star),
+                          Text("4.5 (135 reviews)"),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
